2b0967dc3c568b82b4a6f8fe83087dc66cdbc448
Robert <robert.ryszka.krr@gmail.com>
2021-01-06 23:09:42 +0100

    Fixed stats property

--- src/scraper/scraper.ts
+++ src/scraper/scraper.ts
@@ -9,7 +9,7 @@ export const scrap = async (season: currentHistorical) => {
     const players = await fetchPlayersUrl(season);
 
     for (let player of players) {
-        const playerObject = await fetchPlayerData(player);
+        const playerObject = await fetchPlayerData(player, season);
         savePlayer(playerObject, season);
     }
 };
@@ -17,7 +17,6 @@ const fetchPlayersUrl = async (season: currentHistorical): Promise<Array<any[]>>
     const dataUrl = `https://stats.nba.com/stats/playerindex?College=&Country=&DraftPick=&DraftRound=&DraftYear=&Height=&Historical=${
         season === "current" ? 0 : 1
     }&LeagueID=00&Season=2020-21&SeasonType=Regular%20Season&TeamID=0&Weight=`;
-
     const response = await axios({
         method: "GET",
         headers: { Referer: "https://www.nba.com/" },
@@ -27,10 +26,11 @@ const fetchPlayersUrl = async (season: currentHistorical): Promise<Array<any[]>>
     const data: NbaApiResponseData = response.data;
     return data.resultSets[0].rowSet;
 };
-const fetchPlayerData = async (player: any[]): Promise<Player> => {
+const fetchPlayerData = async (player: any[], season: currentHistorical): Promise<Player> => {
+    const isActive = season === "current" ? true : false;
     const playerId = player[0];
-    const firstName = player[2];
     const lastName = player[1];
+    const firstName = player[2];
     const playerImage = `https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/1040x760/${playerId}.png`;
     const team = `${player[7]} ${player[8]} - ${player[9]}`;
     const height = player[12];
@@ -38,7 +38,12 @@ const fetchPlayerData = async (player: any[]): Promise<Player> => {
     const school = player[14];
     const country = player[15];
     const draft = player[16];
-    const stats = { ppg: player[20], rpg: player[21], apg: player[22] };
+    const stats = {
+        ppg: isActive ? player[22] : player[20],
+        rpg: isActive ? player[23] : player[21],
+        apg: isActive ? player[24] : player[22],
+    };
+
     return {
         firstName,
         lastName,
