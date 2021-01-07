f62bf4e053b350091d82cfb86b0a7ed13c1cf1f6
Robert <robert.ryszka.krr@gmail.com>
2021-01-07 19:07:11 +0100

    Added controller and route for players

--- src/controllers/players.ts
+++ src/controllers/players.ts
@@ -1,7 +1,12 @@
 import mongoose from "mongoose";
 import { RequestHandler } from "express";
 import { currentHistorical } from "../types/season";
+import { CurrentPlayerProfile } from "../models/PlayerProfile";
 
-export const players: RequestHandler = (req, res, next) => {
+export const players: RequestHandler = async (req, res, next) => {
     const season = req.params.season as currentHistorical;
+    if (season === "current") {
+        const players = await CurrentPlayerProfile.find({});
+        res.status(200).json(players);
+    }
 };
--- src/routes/players.ts
+++ src/routes/players.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { checkSeason } from "../middleware/checkSeason";
 import { players } from "../controllers/players";
+
 const router = express.Router();
 
 router.get("/:season", checkSeason, players);
