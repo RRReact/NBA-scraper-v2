99b20db2c0317c2726cccaa1a8352e7c02894f70
Robert <robert.ryszka.krr@gmail.com>
2021-01-07 19:36:12 +0100

    Added historical route to players route

--- src/controllers/players.ts
+++ src/controllers/players.ts
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
 import { RequestHandler } from "express";
 import { currentHistorical } from "../types/season";
-import { CurrentPlayerProfile } from "../models/PlayerProfile";
+import { CurrentPlayerProfile, HistoricalPlayerProfile } from "../models/PlayerProfile";
 
 export const players: RequestHandler = async (req, res, next) => {
     const season = req.params.season as currentHistorical;
     if (season === "current") {
         const players = await CurrentPlayerProfile.find({});
+        res.status(200).json(players);
+    } else {
+        const players = await HistoricalPlayerProfile.find({});
         res.status(200).json(players);
     }
 };
