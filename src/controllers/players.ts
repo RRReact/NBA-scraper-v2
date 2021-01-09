983a384c07512e985c4d3a59c888df80565e6661
Robert <robert.ryszka.krr@gmail.com>
2021-01-09 15:01:35 +0100

    Refactored players controller

--- src/controllers/players.ts
+++ src/controllers/players.ts
@@ -1,15 +1,10 @@
-import mongoose from "mongoose";
 import { RequestHandler } from "express";
 import { currentHistorical } from "../types/season";
 import { CurrentPlayerProfile, HistoricalPlayerProfile } from "../models/PlayerProfile";
 
 export const players: RequestHandler = async (req, res, next) => {
     const season = req.params.season as currentHistorical;
-    if (season === "current") {
-        const players = await CurrentPlayerProfile.find({});
-        res.status(200).json(players);
-    } else {
-        const players = await HistoricalPlayerProfile.find({});
-        res.status(200).json(players);
-    }
+
+    const players = season === "current" ? await CurrentPlayerProfile.find({}) : await HistoricalPlayerProfile.find({});
+    res.status(200).json(players);
 };
