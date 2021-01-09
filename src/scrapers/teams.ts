4c1d8ea3f2fac376b09196b95f42df2c757f9936
Robert <robert.ryszka.krr@gmail.com>
2021-01-09 17:36:14 +0100

    Added fetchTeamData

--- src/scrapers/teams.ts
+++ src/scrapers/teams.ts
@@ -3,17 +3,40 @@ import { TeamProfile } from "../models/TeamProfile";
 import cheerio from "cheerio";
 
 export const scrapTeams = async () => {
-    const players = await fetchTeamsUrl();
-    // for (let player of players) {
-    //     const playerObject = await fetchPlayerData(player, season);
-    //     savePlayer(playerObject, season);
-    // }
+    const teams = await fetchTeamsUrl();
+    for (let team of teams) {
+        const teamObject = await fetchTeamData(team);
+        // savePlayer(playerObject, season);
+    }
 };
 const fetchTeamsUrl = async () => {
     const teamsIdsUrl = "https://neulionms-a.akamaihd.net/nbad/player/v1/nba/site_spa/config.json";
+    const nbaTeams: TeamResponse[] = [];
+
     const response = await axios.get(teamsIdsUrl);
-    const resourceIds = response.data.TEAM_TVE_RESOURCE_IDS;
-    const processedIds = resourceIds.map((id: string) => {
+    const resourceIds: string[] = response.data.TEAM_TVE_RESOURCE_IDS;
+    const ids = resourceIds.map((id: string) => {
         return id.replace("NBATP-", "");
     });
+    const unfilteredTeams: TeamRecord = response.data.teams;
+    ids.forEach((id: string) => {
+        if (unfilteredTeams[id]) {
+            nbaTeams.push(unfilteredTeams[id]);
+        }
+    });
+    return nbaTeams;
 };
+
+const fetchTeamData = async (team: TeamResponse) => {};
+
+interface TeamResponse {
+    city: string;
+    code: string;
+    color: string;
+    conference: string;
+    divison: string;
+    name: string;
+}
+interface TeamRecord {
+    [T: string]: TeamResponse;
+}
