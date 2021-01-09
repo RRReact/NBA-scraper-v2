e1c32021020fd28108edbc8a6d2dafc4ac39d57e
Robert <robert.ryszka.krr@gmail.com>
2021-01-09 15:00:13 +0100

    Added historical players to controller

--- src/controllers/player.ts
+++ src/controllers/player.ts
@@ -5,11 +5,14 @@ import { CurrentPlayerProfile, HistoricalPlayerProfile } from "../models/PlayerP
 export const player: RequestHandler = async (req, res, next) => {
     const { firstName, lastName }: PlayerRequest = req.body;
     const season = req.params.season;
-    if (season === "current") {
-        const player = await CurrentPlayerProfile.findOne({ firstName, lastName });
-        if (!player) {
-            res.status(404).json({ message: "Player not found" });
-        }
+    const player =
+        season === "current"
+            ? await CurrentPlayerProfile.findOne({ firstName, lastName })
+            : await HistoricalPlayerProfile.findOne({ firstName, lastName });
+
+    if (!player) {
+        res.status(404).json({ message: "Player not found" });
+    } else {
         res.status(200).json(player);
     }
 };
