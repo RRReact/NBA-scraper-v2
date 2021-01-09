2ede2bae1bfa3101dfb305375905a1ad37f690a6
Robert <robert.ryszka.krr@gmail.com>
2021-01-09 14:55:10 +0100

    Added player controller

--- src/controllers/player.ts
+++ src/controllers/player.ts
@@ -1,6 +1,15 @@
 import { RequestHandler } from "express";
 import { PlayerRequest } from "../types/player";
+import { CurrentPlayerProfile, HistoricalPlayerProfile } from "../models/PlayerProfile";
 
 export const player: RequestHandler = async (req, res, next) => {
     const { firstName, lastName }: PlayerRequest = req.body;
+    const season = req.params.season;
+    if (season === "current") {
+        const player = await CurrentPlayerProfile.findOne({ firstName, lastName });
+        if (!player) {
+            res.status(404).json({ message: "Player not found" });
+        }
+        res.status(200).json(player);
+    }
 };
